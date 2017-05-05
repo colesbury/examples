87b4f7fc312be633f544cc5cdd2e2dccb8869a67
Sam Gross <sgross@fb.com>
2017-05-05 09:00:43 -0700

    Resume training for extra 30 epochs

--- imagenet/launch_jobs.sh
+++ imagenet/launch_jobs.sh
@@ -3,6 +3,7 @@ IMAGENET_DIR=${IMAGENET_DIR:-/data/local/packages/ai-group.imagenet-full-size/pr
 NUM_LOCAL=$1
 NUM_REPLICAS=${2:-$1}
 LR=${3:-0.1}
+RESUME=/mnt/vol/gfsai-east/ai-group/users/sgross/resnet-nccl2/resnet50-8x8x32-async.pt
 if [ -z $NUM_LOCAL ]; then
   echo "usage: ./launch_jobs.sh NUM_LOCAL [NUM_REPLICAS] [LR]"
   exit 1
@@ -11,6 +12,6 @@ for i in $(seq 0 $((NUM_LOCAL-1)))
 do
   echo $i
   CHECKPOINT_DIR=replica$i
-  python3 -u main.py -a resnet50 --num-replicas $NUM_REPLICAS -b 32 --lr $LR --checkpoint-dir $CHECKPOINT_DIR $IMAGENET_DIR &>replica$i.log &
+  python3 -u main.py -a resnet50 --num-replicas $NUM_REPLICAS --epochs 120 --resume $RESUME -b 32 --lr $LR --checkpoint-dir $CHECKPOINT_DIR $IMAGENET_DIR &>replica$i.log &
 done
 wait
