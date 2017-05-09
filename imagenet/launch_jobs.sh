63ca986af3cc9db9640c72905d598ac5e2e69f93
Sam Gross <sgross@fb.com>
2017-05-09 13:24:16 -0700

    Treat first 20 epochs as 5

--- imagenet/launch_jobs.sh
+++ imagenet/launch_jobs.sh
@@ -3,7 +3,7 @@ IMAGENET_DIR=${IMAGENET_DIR:-/data/local/packages/ai-group.imagenet-full-size/pr
 NUM_LOCAL=$1
 NUM_REPLICAS=${2:-$1}
 LR=${3:-0.1}
-#RESUME=/data/users/sgross/resnet50-epoch5.pt
+RESUME=/data/users/sgross/resnet50-8x8x32-epoch20.pt
 if [ -z $NUM_LOCAL ]; then
   echo "usage: ./launch_jobs.sh NUM_LOCAL [NUM_REPLICAS] [LR]"
   exit 1
@@ -12,6 +12,6 @@ for i in $(seq 0 $((NUM_LOCAL-1)))
 do
   echo $i
   CHECKPOINT_DIR=replica$i
-  python3 -u main.py -a resnet50 --num-replicas $NUM_REPLICAS --epochs 20 -b 32 --lr $LR --checkpoint-dir $CHECKPOINT_DIR $IMAGENET_DIR &>replica$i.log &
+  python3 -u main.py -a resnet50 --num-replicas $NUM_REPLICAS --resume $RESUME --epochs 135 -b 32 --lr $LR --checkpoint-dir $CHECKPOINT_DIR $IMAGENET_DIR &>replica$i.log &
 done
 wait
--- imagenet/main.py
+++ imagenet/main.py
@@ -428,7 +428,7 @@ class AverageMeter(object):
 
 def adjust_learning_rate(optimizer, epoch):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    lr = args.lr * (0.1 ** (epoch // 30))
+    lr = args.lr * (0.1 ** ((epoch - 15) // 30))
     print('Set learning rate to {:f}'.format(lr))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
